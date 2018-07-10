c906de8f2115f09f985885fb4efe0078f715aa7d
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-07-10 14:56:34 +0300

    Compilation script

---
+++ specification/compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SRC=UAVCAN_Specification
+
+#rm -rf out &> /dev/null
+mkdir out &> /dev/null
+cp -fP *.bib out/ &> /dev/null
+
+rm out/$SRC.pdf
+
+pdflatex --halt-on-error --shell-escape -output-directory=out ../$SRC.tex
+cd out
+bibtex $SRC
+#biber $SRC
+cd ..
+pdflatex --halt-on-error --shell-escape -output-directory=out ../$SRC.tex
+pdflatex --halt-on-error --shell-escape -output-directory=out ../$SRC.tex
+
+rm *.pdf*
