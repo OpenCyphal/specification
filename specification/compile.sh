ed18a7be2388b3f511d5800d58394d6a5394d605
Kjetil Kjeka <kjetilkjeka@gmail.com>
2018-08-19 16:31:35 +0200

    Have travis build pdfs. Usefull for PR's and generating documentation from master branch

--- specification/compile.sh
+++ specification/compile.sh
@@ -7,12 +7,20 @@ mkdir out &> /dev/null
 cp -fP *.bib out/ &> /dev/null
 
 rm out/$SRC.pdf
+rm out/$SRC.log
 
-pdflatex --halt-on-error --shell-escape -output-directory=out ../$SRC.tex
+pdflatex --halt-on-error --shell-escape $SRC.tex
 #cd out
 #bibtex $SRC
 #cd ..
-pdflatex --halt-on-error --shell-escape -output-directory=out ../$SRC.tex
-pdflatex --halt-on-error --shell-escape -output-directory=out ../$SRC.tex
+pdflatex --halt-on-error --shell-escape $SRC.tex
+pdflatex --halt-on-error --shell-escape $SRC.tex
 
-rm *.pdf*
+mv $SRC.pdf out/$SRC.pdf
+mv $SRC.log out/$SRC.log
+
+rm $SRC.aux
+rm $SRC.lof
+rm $SRC.lot
+rm $SRC.out
+rm $SRC.toc
