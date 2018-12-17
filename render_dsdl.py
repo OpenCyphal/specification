81f99b8730c993c0d37e8dfb5e72b976a8b8fc6e
Pavel Kirienko <pavel.kirienko@gmail.com>
2018-12-17 20:05:24 +0200

    Started the section 5.2 - application-level functions. Added more latex generation logic into the DSDL rendering script

--- render_dsdl.py
+++ render_dsdl.py
@@ -24,7 +24,7 @@ def escape(s: str) -> str:
     return s.replace('_', r'\_')
 
 
-def render_dsdl_definition(t: pydsdl.data_type.CompoundType) -> str:
+def render_dsdl_length_table(t: pydsdl.data_type.CompoundType) -> str:
     def bit_to_byte(val: tuple) -> tuple:
         return tuple((x + 7) // 8 for x in val)
 
@@ -56,15 +56,19 @@ def render_dsdl_definition(t: pydsdl.data_type.CompoundType) -> str:
     else:
         length_table_rows = 'Message length &' + '&'.join(rlen_group(t.bit_length_range)) + r'\\'
 
-    minted_params = r'fontsize=\scriptsize, numberblanklines=true, baselinestretch=0.9, autogobble=false'
     return '\n'.join([
         r'{\footnotesize',
         r'\begin{tabu}{|r|c c c c|}\hline',
         r'Length unit & Bit & Byte (octet) & CAN MTU 8 & CAN MTU 64 \\\hline',
         length_table_rows,
         r'\hline\end{tabu}',
-        r'}',
-        r'\pagebreak[2]{}',    # This is needed to discourage page breaks within the listings
+        r'}'
+    ])
+
+
+def render_dsdl_definition(t: pydsdl.data_type.CompoundType) -> str:
+    minted_params = r'fontsize=\scriptsize, numberblanklines=true, baselinestretch=0.9, autogobble=false'
+    return '\n'.join([
         r'\begin{minted}[%s]{python}' % minted_params,
         open(t.source_file_path).read(),
         r'\end{minted}',
@@ -94,8 +98,39 @@ matching = list(filter(lambda t: fnmatch(t.full_name, pattern), parsed_types))
 if not matching:
     die('No types match the pattern: %s' % pattern)
 
-# Sort alphabetically and by version
-matching = list(sorted(matching, key=str))
+# Sort by name and by version; note that the version numbers must be sorted numerically, not lexicographically.
+matching = list(sorted(matching, key=lambda t: '%s%03d%03d' % (t.full_name, t.version.major, t.version.minor)))
+
+# See if we were asked to render a particular type only.
+# If that is the case, output an abridged form, provide a reference, and then exit.
+# The output should contain the latest non-deprecated definition.
+if '*' not in pattern:
+    matching_except_deprecated = list(filter(lambda t: not t.deprecated, matching))
+    if not matching_except_deprecated:
+        die('All versions of the type %s are deprecated, nothing to display' % pattern)
+
+    t = matching_except_deprecated[-1]       # Due to sorting, latest version ends up last
+    service_or_subject = 'service' if isinstance(t, pydsdl.data_type.ServiceType) else 'subject'
+    print(r'DSDL source text of \verb|%s|' % t.full_name)
+    print('version %d.%d' % t.version)
+    if len(matching) > 2:
+        print('(there are', len(matching) - 1, 'older versions)')
+    elif len(matching) > 1:
+        print('(there is one older version)')
+    else:
+        print('(this is the only version)')
+
+    if t.has_fixed_port_id:
+        print('with fixed', service_or_subject, 'ID', t.fixed_port_id)
+    else:
+        print('without fixed', service_or_subject, 'ID')
+
+    print(r'is provided below.')
+    print(r'More information is available in')
+    print(r'section \ref{sec:dsdl:%s} on page \pageref{sec:dsdl:%s}.' % (t.full_name, t.full_name))
+    print(r'\pagebreak[2]{}')           # This is needed to discourage page breaks within the listings
+    print(render_dsdl_definition(t))
+    exit(0)
 
 # Group by namespace and by type name (i.e., all versions grouped together by name)
 grouped = OrderedDict()
@@ -168,4 +203,6 @@ for namespace, children in grouped.items():
                 title += ', DEPRECATED'
 
             print(r'\subsubsection{%s}' % title)
+            print(render_dsdl_length_table(t))
+            print(r'\pagebreak[2]{}')           # This is needed to discourage page breaks within the listings
             print(render_dsdl_definition(t))
