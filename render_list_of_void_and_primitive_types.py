fd5c6b00910db1661a72164005e9a24fffef7e2e
Pavel Kirienko <pavel.kirienko@gmail.com>
2019-05-05 20:20:00 +0300

    Allow 1-bit unsigned integers.
    One-bit integers allow us to eliminate special cases in the serialization definitions for variable arrays and unions.

--- render_list_of_void_and_primitive_types.py
+++ render_list_of_void_and_primitive_types.py
@@ -8,7 +8,7 @@ for i in range(1, 65):
     print(' ' * 4 + r'\item ' + r' \quad{} '.join(filter(None, [
         (r'\verb|void%-3d|') % i,
         (r'\verb|int%-3d|'   % i) if i >= 2 else None,
-        (r'\verb|uint%-3d|'  % i) if i >= 2 else None,
+        (r'\verb|uint%-3d|'  % i) if i >= 1 else None,
         (r'\verb|float%-3d|' % i) if i in (16, 32, 64) else None,
         (r'\verb|bool|') if i == 1 else None,
     ])))
